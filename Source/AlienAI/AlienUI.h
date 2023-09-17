3c05c879173bb053dff2a59bf5262fe499d8b9a8
tompsognathus <tompsognathus@gmail.com>
2023-09-17 13:10:40 +0100

    added player targeting gauge

---
+++ Source/AlienAI/AlienUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AlienUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALIENAI_API UAlienUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
